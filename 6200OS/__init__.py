7132b776a0eb3411414bcf3cad7605439cdbf8c7
Siyuan Li <siyuan@Siyuans-MacBook-Pro.local>
2019-09-05 22:12:48 -0400

    20190905 pm commit

--- 6200OS/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  5 10:55:15 2019
-
-@author: sli
-"""
-
--- 7642RL/HW1.py
+++ 7642RL/HW1.py
@@ -6,21 +6,37 @@ Created on Thu Sep  5 09:49:21 2019
 """
 import sys
 import numpy as np
-def DieN():
-    N=int(sys.argv[1])
-    isBadList=(sys.argv[2].split(','))
-    isBadList=np.array([int(i) for i in isBadList])
-    die=np.arange(1,N+1)
-    print(die*((1-isBadList)/N))
-    bankroll=0
+N=int(sys.argv[1])
+isBadList=(sys.argv[2].split(','))
+isBadList=np.array([int(i) for i in isBadList])
+die=np.arange(1,N+1)
+
+
+def threshold(N,isBadList):
     P_bad=sum(isBadList)/N
-    while -bankroll*P_bad+sum(die*((1-isBadList)/N))>0:#
-        print(bankroll)
-        bankroll=bankroll-bankroll*P_bad+sum(die*((1-isBadList)/N))
+    die=np.arange(1,N+1)
+    return sum(die*((1-np.array(isBadList))/N))/P_bad
+
+def DieN(N,isBadList,money_start):
+    die=np.arange(1,N+1)
+    dieList=die*(1-np.array(isBadList))
+    stopSignal=threshold(N,isBadList)
+    out=np.zeros(N)
+    if money_start<=stopSignal:
+        for i in range(len(dieList)):
+            if dieList[i] ==0:
+                out[i]=0
+            else:
+                money_now=money_start+dieList[i]
+                out[i]=DieN(N,isBadList,money_now)
+    else: 
+        return money_start
+    
+    return np.mean(out)
     
     
-    print (bankroll)
-    return
+    
 
 if __name__=="__main__":
-    DieN()
+    print(DieN(N,isBadList,money_start=0))
+
